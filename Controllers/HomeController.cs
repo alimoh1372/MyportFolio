1bd29a16772ad81239a57ccfa5ce46d80a8c3699
Ali From NocEmam <alimohammadzade7976@gmail.com>
2022-12-06 05:50:15 +0330

    1401/09/15

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,19 +11,12 @@ namespace MyportFolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
