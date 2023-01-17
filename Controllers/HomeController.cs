9cc3f53c47161bac8b91900b6c0929d5e9ea3bed
Ali From NocEmam <alimohammadzade7976@gmail.com>
2023-01-17 03:50:35 +0330

    1401/10/27 03:51

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,20 +11,40 @@ namespace MyportFolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<Portfolio> _portfolios= new List<Portfolio>()
+        private readonly List<Portfolio> _portfolios = new List<Portfolio>()
         {
-            new Portfolio{Id = 1,Image ="01.jpg" ,Title = "Accounting",Description = "یک برنامه برای انجام امور حسابداری شخصی تحت ویندوز"},
+            new Portfolio
+            {
+                Id = 1, Image = "01.jpg", Title = "Accounting",
+                Description = "یک برنامه برای انجام امور حسابداری شخصی تحت ویندوز"
+            },
 
-            new Portfolio{Id = 2,Image ="02.jpg" ,Title = "مدیریت مدرسه",Description = "کمک به مدیریت مدرسه تمام داینامیک جهت اضافه کردن دانش اموزان معلمین و سایر آبجکت های مدرسه نحت ویندوز"},
+            new Portfolio
+            {
+                Id = 2, Image = "02.jpg", Title = "مدیریت مدرسه",
+                Description =
+                    "کمک به مدیریت مدرسه تمام داینامیک جهت اضافه کردن دانش اموزان معلمین و سایر آبجکت های مدرسه نحت ویندوز"
+            },
 
-            new Portfolio { Id = 3, Image = "03.jpg", Title = " سایت فروشگاه", Description = "سایت فروشگاه به تحت MVC و مدیریت محصولات دسته بندی محصولات و ..." },
+            new Portfolio
+            {
+                Id = 3, Image = "03.jpg", Title = " سایت فروشگاه",
+                Description = "سایت فروشگاه به تحت MVC و مدیریت محصولات دسته بندی محصولات و ..."
+            },
 
 
-            new Portfolio { Id = 4, Image = "04.jpg", Title = "مدیریت ساختمان", Description = "برنامه ساده و کاربردی به جهت مدیریت امور ساختمان و هزینه های آن" },
-
+            new Portfolio
+            {
+                Id = 4, Image = "04.jpg", Title = "مدیریت ساختمان",
+                Description = "برنامه ساده و کاربردی به جهت مدیریت امور ساختمان و هزینه های آن"
+            },
         };
-public IActionResult Index()
+
+        public IActionResult Index()
         {
+            ViewBag.headingTitle = "سایت شخصی نمونه کارهای علی محمدزاده";
+            ViewData["headingText"] =
+                "این سایت به جهت معرفی نمونه کارهای اینجانب برای دوستان و مشتریان عزیز فراهم شده است تا قبل از استفاده از خدمات برنامه نویسی بتوانند نمونه کارها را مشاهده و با خیال راحت سفارش بدهند";
             return View(_portfolios);
         }
 
@@ -42,14 +62,16 @@ public IActionResult Index()
                 Number = "09383738757",
                 Github = "Github.com/alimoh1372"
             };
-           var result= Json(data: contact);
+            var result = Json(data: contact);
             return result;
         }
+
         [HttpGet]
         public JsonResult GetPortfolios()
         {
             return Json(data: _portfolios);
         }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
